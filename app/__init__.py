c242a77dd9e2b48282edb9ddcbf51567d4972dec
Arush Kathal <165474641+ayorush1@users.noreply.github.com>
2025-05-03 19:08:57 +0800

    Basic SQLAlchemy Setup
    Added the basic setup to link SQL database to Flask via SQLAlchemy.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
-# This initialises the flask instance
-# Because it's labelled as __init__ it gets run on package start
-
 from flask import Flask
+
+#SQL Database imports
+from flask_migrate import Migrate 
+from flask_sqlalchemy import SQLAlchemy 
+from app.config import Config 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here' #secret key added for session cookies
+app.config.from_object(Config) 
+db = SQLAlchemy(app) 
+migrate = Migrate(app, db) 
 
 
 from app import routes # This is imported later to avoid circular referencing issues with Flask
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db 
+
+
+# Table stores account user information such as a user id, username and password. 
+class Accounts(db.Model): 
+    user_id = db.Column(db.Integer, primary_key=True) 
+    user_name = db.Column(db.String(50), nullable=False)
+    #password =
+
