44a797617b03d0dd047ee3ad003ac6f2fb9ce324
eternits <nityatewari10@gmail.com>
2025-05-04 15:13:03 +0800

    Add tailwind.config.js for custom Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+      './app/templates/**/*.html',    
+    ],
+    theme: {
+      extend: {
+        colors: {
+          darkbg: '#0f172a',
+          lighttext: '#f8fafc',
+          accent: '#38bdf8',
+          hover: '#a312e6',
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
