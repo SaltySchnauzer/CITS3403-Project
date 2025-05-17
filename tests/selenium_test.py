e31c0d3504a3af548fc236cb1edf13a2d90f430b
SaltySchnauzer <52157258+SaltySchnauzer@users.noreply.github.com>
2025-05-17 23:02:30 +0800

    Selenium Testing finally function
    Thanks to Ethan for some help + code - not all of this is my work :)

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,12 +1,14 @@
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import threading
+from selenium.webdriver.firefox.options import Options
+from multiprocessing import Process
 import time
 from app.config import TestConfig
 from app import create_app, db
 from app.models import User
+
+base_url = ""
 
 def add_test_data():
         # Create a test user
@@ -24,15 +26,13 @@ class SeleniumTestCase(TestCase):
         db.create_all()
         add_test_data()
 
-        # Start server thread
-        def run():
-            self.testApp.run(port=5001, use_reloader=False)
-
-        self.server_thread = threading.Thread(target=run)
+        self.server_thread = Process(target=self.testApp.run)
         self.server_thread.start()
 
         # Set up headless Chrome browser
-        self.driver = webdriver.Firefox()
+        foxOptions = Options()
+        foxOptions.add_argument('-headless')
+        self.driver = webdriver.Firefox(options=foxOptions)
 
     def tearDown(self):
         self.server_thread.terminate()
@@ -41,11 +41,38 @@ class SeleniumTestCase(TestCase):
         db.drop_all()
         self.app_context.pop()
 
-    def test_login_success(self):
-        self.driver.get("http://localhost:5001/signin")
-        self.driver.find_element(By.NAME, "username").send_keys("testuser")
-        self.driver.find_element(By.NAME, "password").send_keys("password123")
-        self.driver.find_element(By.CSS_SELECTOR, "form button").click()
+    def test_signin_button(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.find_element(By.LINK_TEXT, "Log In").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/signin"
 
-        # Check that we're redirected to the session page or homepage
-        self.assertIn("session", self.browser.current_url)
+    def test_login_required(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.find_element(By.LINK_TEXT, "History").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/signin?next=%2Fhistory"
+
+    def test_login(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.find_element(By.LINK_TEXT, "Log In").click()
+        self.driver.find_element(By.ID, "username").send_keys("testuser")
+        self.driver.find_element(By.ID, "password").send_keys("password123")
+        self.driver.find_element(By.ID, "submit").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/index"
+    
+    def test_gated_buttons(self):
+        self.driver.get("http://127.0.0.1:5000/signin")
+        self.driver.find_element(By.ID, "username").send_keys("testuser")
+        self.driver.find_element(By.ID, "password").send_keys("password123")
+        self.driver.find_element(By.ID, "submit").click()
+        self.driver.find_element(By.LINK_TEXT, "Friends").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/friends"
+        self.driver.find_element(By.LINK_TEXT, "History").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/history"
+  
+    def test_signup(self):
+        self.driver.get("http://127.0.0.1:5000/signup")
+        self.driver.find_element(By.ID, "username").send_keys("newuser")
+        self.driver.find_element(By.ID, "password").send_keys("newpwd")
+        self.driver.find_element(By.ID, "password2").send_keys("newpwd")
+        self.driver.find_element(By.ID, "submit").click()
+        assert self.driver.current_url == "http://127.0.0.1:5000/index"
